b4951ec8e6e548e8f90c195466d99ce9063f762b
vinilco <vinilco2@gmail.com>
2024-05-13 20:49:10 +0300

    initial commit

---
+++ internal/event_manager/errors.go
@@ -0,0 +1,17 @@
+package event_manager
+
+import "errors"
+
+var EventFormatError = errors.New("event format error")
+var YouShallNotPass = errors.New("YouShallNotPass")
+var NotOpenYet = errors.New("NotOpenYet")
+var PlaceIsBusy = errors.New("PlaceIsBusy")
+var ClientUnknown = errors.New("ClientUnknown")
+var ICanWaitNoLonger = errors.New("ICanWaitNoLonger!")
+
+var QueueIsFull = errors.New("queue is full")
+var QueueIsEmpty = errors.New("queue is empty")
+var ClientNotInQueue = errors.New("queue does not contain client")
+
+var DeskIsOccupied = errors.New("desk is occupied")
+var DeskAlreadyFree = errors.New("desk is already free")
